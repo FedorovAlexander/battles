e0bc2131bb5e98e505ab5f0fe9be8d89284cbaa4
Alexander Fedorov <hamster2240@gmail.com>
2023-11-02 21:05:03 +0200

    Format page.svelte file. Add onDestroy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,29 @@
-<div id="map"></div>
 <script>
-	import mapboxgl from "mapbox-gl";
-	import "../../node_modules/mapbox-gl/dist/mapbox-gl.css"
-	import { onMount } from "svelte";
+	import mapboxgl from 'mapbox-gl';
+	import '../../node_modules/mapbox-gl/dist/mapbox-gl.css';
+	import { onMount, onDestroy } from 'svelte';
 
+	let map;
 	onMount(() => {
-			const mapboxkey = 'pk.eyJ1IjoiYWxleDIyNDAiLCJhIjoiY2xvMDNjYjFnMTRycDJubzZlc3NnbG56byJ9.fVFXHiJm2WS5M33-4gH20g';
-			mapboxgl.accessToken = mapboxkey;
-		let map = new mapboxgl.Map({
-			container: "map",
-			style: "mapbox://styles/mapbox/streets-v11",
+		const mapboxkey = 'pk.eyJ1IjoiYWxleDIyNDAiLCJhIjoiY2xvMDNjYjFnMTRycDJubzZlc3NnbG56byJ9.fVFXHiJm2WS5M33-4gH20g';
+		mapboxgl.accessToken = mapboxkey;
+
+		map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
 			center: [-74.5, 40],
 			zoom: 9,
 		});
 	});
+
+	onDestroy(() => {
+		if (map) {
+			map.remove();
+		}
+	});
 </script>
+
+<div id="map" />
 
 <style>
 	#map {
