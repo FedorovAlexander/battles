63c6be03fbcd3a20440c7b7538d6cd0873d7f3a5
Alexander Fedorov <hamster2240@gmail.com>
2023-11-03 16:17:40 +0200

    Fetch data and create markers on the map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import { onMount, onDestroy } from 'svelte';
 
 	let map;
+
 	onMount(() => {
 		const mapboxkey = 'pk.eyJ1IjoiYWxleDIyNDAiLCJhIjoiY2xvMDNjYjFnMTRycDJubzZlc3NnbG56byJ9.fVFXHiJm2WS5M33-4gH20g';
 		mapboxgl.accessToken = mapboxkey;
@@ -11,8 +12,18 @@
 		map = new mapboxgl.Map({
 			container: 'map',
 			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [-74.5, 40],
-			zoom: 9,
+			center: [10.451526, 51.165691],
+			zoom: 1,
+		});
+
+		map.on('load', () => {
+			fetchBattlesData().then((data) => {
+				data.results.bindings.forEach((battle) => {
+					if (battle.latitude && battle.longitude) {
+						addMarker(battle);
+					}
+				});
+			});
 		});
 	});
 
@@ -21,6 +32,61 @@
 			map.remove();
 		}
 	});
+
+	async function fetchBattlesData() {
+		const sparqlQuery = `
+		SELECT ?battle ?battleLabel ?date ?startDate ?endDate ?latitude ?longitude ?casualties ?image
+		WHERE {
+				?battle wdt:P31 wd:Q178561.
+				OPTIONAL { ?battle wdt:P585 ?date. }
+				OPTIONAL { ?battle wdt:P580 ?startDate. }
+				OPTIONAL { ?battle wdt:P582 ?endDate. }
+				OPTIONAL { ?battle wdt:P625 ?coordinates. }
+				OPTIONAL {
+						?battle wdt:P625 ?coord.
+						BIND (xsd:double(SUBSTR(str(?coord), 7, 10)) AS ?longitude).
+						BIND (xsd:double(SUBSTR(str(?coord), 18, 10)) AS ?latitude).
+				}
+				OPTIONAL { ?battle wdt:P1120 ?casualties. }
+				OPTIONAL { ?battle wdt:P18 ?image. }
+				SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+		}
+	`;
+
+		const endpoint = 'https://query.wikidata.org/sparql';
+		const url = endpoint + '?query=' + encodeURIComponent(sparqlQuery) + '&format=json';
+
+		try {
+			const response = await fetch(url, { method: 'GET' });
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	}
+
+	function addMarker(data) {
+		const el = document.createElement('div');
+		el.className = 'marker';
+		el.style.backgroundColor = '#ff0000';
+		el.style.width = '10px';
+		el.style.height = '10px';
+		el.style.borderRadius = '50%';
+		el.addEventListener('click', () => {
+			console.log(data);
+		});
+
+		const coordinates = data.latitude && data.longitude ? [data.longitude.value, data.latitude.value] : null;
+		if (coordinates) {
+			const marker = new mapboxgl.Marker(el).setLngLat(coordinates).addTo(map);
+		} else {
+			console.log('no coordinates', data);
+		}
+	}
 </script>
 
 <div id="map" />
